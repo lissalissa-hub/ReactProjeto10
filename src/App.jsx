ef417cc133aa864894dc86381b40fe52527c68ff
lissalissa-hub <melissadias748@gmail.com>
2024-10-20 18:47:43 -0300

    Atividade10

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Botao from './components/Botao'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Botao logado={true}/>
+    <Botao logado={false}/>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Botao.jsx
@@ -0,0 +1,6 @@
+function Botao (props) {
+    return props.logado?<button type="button">Entrar</button>:<button type ="button">Sair</button>
+
+}
+
+export default Botao
